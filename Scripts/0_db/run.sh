424f14d4659da71bdabd38f5f3c80f09660fa93a
José Alexandre Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2026-01-09 23:44:45 -0300

    Update run.sh

--- Scripts/0_db/run.sh
+++ Scripts/0_db/run.sh
@@ -1,27 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xe
 
 process_file() {
     local fasta="$1"
     local fname=$(basename "$fasta")
     local prefix=${fname%.nt.fas}
     local tree="./nwk/${prefix}.nwk"
-    
+
     if [[ -f "$tree" ]]; then
         echo "Processando $prefix"
-        hhfilter -i "./nt/${prefix}.nt.fas" -o /dev/stdout -id 99 -M 50 | \
-            reformat.pl a3m fas /dev/stdin /dev/stdout -uc > "./nt_filtered/${prefix}.nt.filtered.fas"
+        trimal -maxidentity 0.95 -in "./nt/${prefix}.nt.fas" -out "./nt_filtered/${prefix}.nt.filtered.fas"
         gotree prune -r -f <(seqkit fx2tab -ni "./nt_filtered/${prefix}.nt.filtered.fas") \
             -i "$tree" > "./nwk_filtered/${prefix}.filtered.nwk"
         seqkit grep -f <(gotree labels -i "./nwk_filtered/${prefix}.filtered.nwk") \
             "./nt_filtered/${prefix}.nt.filtered.fas" > "./nt_tree_filtered/${prefix}.nt.filtered.fas"
-        hyphy fubar --code Universal \
+        hyphy fubar \
+            --code $(./get_code.py "./nt_tree_filtered/${prefix}.nt.filtered.fas") \
             --alignment "./nt_tree_filtered/${prefix}.nt.filtered.fas" \
             --tree "./nwk_filtered/${prefix}.filtered.nwk" \
             --output "./output/${prefix}.filtered.fubar.json" \
             > ./err/hyphy_errors_$prefix.log.txt 2>&1
         rm "./nt_tree_filtered/${prefix}.nt.filtered.fas.FUBAR.cache" 2>/dev/null
     else
-        echo "${prefix}.nt" >> .err/missing_trees.txt
+        echo "${prefix}.nt" >> ./err/missing_trees.txt
     fi
 }
 
@@ -29,4 +31,4 @@ export -f process_file
 
 mkdir -p nt_filtered nwk_filtered nt_tree_filtered output
 
-ls ./nt/*.fas | rush -j 12 'process_file {1}'
+ls ./nt/*.fas | rush -j 1 'process_file {1}'
